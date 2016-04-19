0944013b73c6546dece1830ee38acfcebff2b104
Bruce Woodward <bruce.woodward@gmail.com>
2016-04-19 14:30:44 +1000

    ref: remove unused key_to_id from project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :copies
 
-  scope :key_to_id, -> (key) { where(key: key).select(:id).first }
-
   def self.copy_details_for(project_key)
     key = Project.where(key: project_key).select(:key).first.key
     Copy.where(project_key: key).order('date desc').all
